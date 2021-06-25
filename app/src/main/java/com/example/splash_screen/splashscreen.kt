ac827c73abd6abfca1de7331e13bb2bdeb9efa3c
njerubrian <47820971+njerubrian@users.noreply.github.com>
2021-06-25 09:24:21 +0300

    implememting a splash screen without usinh the layout

---
+++ app/src/main/java/com/example/splash_screen/splashscreen.kt
@@ -0,0 +1,17 @@
+package com.example.splash_screen
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.os.Handler
+
+class splashscreen : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        Handler().postDelayed(
+            { val intent_delayed = Intent(this, MainActivity::class.java)
+                startActivity(intent_delayed)
+            },3000)
+    }
+}
